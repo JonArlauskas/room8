5438452a40da0d6eff88e60c19f682b1b4601d36
Jonathan Arlauskas <jonathan.arlauskas@gmail.com>
2015-09-12 01:16:26 -0400

    app delegate changes for parse

--- room8/AppDelegate.swift
+++ room8/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse
+        Parse.setApplicationId("svdIw7ipYTkR4J5dhixKPpozNiZOy98gPsBVUXbJ", clientKey: "Y2QBiAGj83kDU91PXdxJ6frOo2MPBKqOkrbzw623")
+        
+        // Track statistics around application opens
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
